f2fe7b63360c2d2bdd5119abc1f81931511c5b47
PJ Neary <pneary@pathward.com>
2024-10-21 13:50:36 -0500

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import {
   Image,
   Grid,
   Divider,
+  ThemeProvider,
+  useTheme,
 } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
@@ -21,17 +23,7 @@ import { generateClient } from "aws-amplify/data";
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
 
-Amplify.configure({
-  API: {
-    REST: {
-      AuthAPI: {
-        endpoint:
-          'https://2kij2xrrqi.execute-api.us-east-1.amazonaws.com/test/login',
-        region: 'us-west-2' // Optional
-      }
-    }
-  }
-});
+Amplify.configure();
 
 const client = generateClient({
   authMode: "userPool",
@@ -39,6 +31,46 @@ const client = generateClient({
 
 export default function App() {
   const [notes, setNotes] = useState([]);
+
+  const { tokens } = useTheme();
+  const theme = {
+    name: 'Auth Example Theme',
+    tokens: {
+      components: {
+        authenticator: {
+          router: {
+            boxShadow: `0 0 16px ${tokens.colors.overlay['10']}`,
+            borderWidth: '0',
+          },
+          form: {
+            padding: `${tokens.space.medium} ${tokens.space.xl} ${tokens.space.medium}`,
+          },
+        },
+        button: {
+          primary: {
+            backgroundColor: tokens.colors.neutral['100'],
+          },
+          link: {
+            color: tokens.colors.purple['80'],
+          },
+        },
+        fieldcontrol: {
+          _focus: {
+            boxShadow: `0 0 0 2px ${tokens.colors.purple['60']}`,
+          },
+        },
+        tabs: {
+          item: {
+            color: tokens.colors.neutral['80'],
+            _active: {
+              borderColor: tokens.colors.neutral['100'],
+              color: tokens.colors.purple['100'],
+            },
+          },
+        },
+      },
+    },
+  };
 
   useEffect(() => {
     fetchNotes();
@@ -100,97 +132,100 @@ export default function App() {
   }
 
   return (
-    <Authenticator loginMechanisms={['username']} variation="modal" hideSignUp>
-      {({ signOut }) => (
-        <Flex
-          className="App"
-          justifyContent="center"
-          alignItems="center"
-          direction="column"
-          width="70%"
-          margin="0 auto"
-        >
-          <Heading level={1}>My Notes App</Heading>
-          <View as="form" margin="3rem 0" onSubmit={createNote}>
-            <Flex
-              direction="column"
-              justifyContent="center"
-              gap="2rem"
-              padding="2rem"
-            >
-              <TextField
-                name="name"
-                placeholder="Note Name"
-                label="Note Name"
-                labelHidden
-                variation="quiet"
-                required
-              />
-              <TextField
-                name="description"
-                placeholder="Note Description"
-                label="Note Description"
-                labelHidden
-                variation="quiet"
-                required
-              />
-              <View
-                name="image"
-                as="input"
-                type="file"
-                alignSelf={"end"}
-                accept="image/png, image/jpeg"
-              />
-
-              <Button type="submit" variation="primary">
-                Create Note
-              </Button>
-            </Flex>
-          </View>
-          <Divider />
-          <Heading level={2}>Current Notes</Heading>
-          <Grid
-            margin="3rem 0"
-            autoFlow="column"
+    <ThemeProvider theme={theme}>
+      <View padding="xxl"></View>
+      <Authenticator loginMechanisms={['username']} variation="modal" hideSignUp>
+        {({ signOut }) => (
+          <Flex
+            className="App"
             justifyContent="center"
-            gap="2rem"
-            alignContent="center"
+            alignItems="center"
+            direction="column"
+            width="70%"
+            margin="0 auto"
           >
-            {notes.map((note) => (
+            <Heading level={1}>My Notes App</Heading>
+            <View as="form" margin="3rem 0" onSubmit={createNote}>
               <Flex
-                key={note.id || note.name}
                 direction="column"
                 justifyContent="center"
-                alignItems="center"
                 gap="2rem"
-                border="1px solid #ccc"
                 padding="2rem"
-                borderRadius="5%"
-                className="box"
               >
-                <View>
-                  <Heading level="3">{note.name}</Heading>
-                </View>
-                <Text fontStyle="italic">{note.description}</Text>
-                {note.image && (
-                  <Image
-                    src={note.image}
-                    alt={`visual aid for ${notes.name}`}
-                    style={{ width: 400 }}
-                  />
-                )}
-                <Button
-                  variation="destructive"
-                  onClick={() => deleteNote(note)}
-                >
-                  Delete note
+                <TextField
+                  name="name"
+                  placeholder="Note Name"
+                  label="Note Name"
+                  labelHidden
+                  variation="quiet"
+                  required
+                />
+                <TextField
+                  name="description"
+                  placeholder="Note Description"
+                  label="Note Description"
+                  labelHidden
+                  variation="quiet"
+                  required
+                />
+                <View
+                  name="image"
+                  as="input"
+                  type="file"
+                  alignSelf={"end"}
+                  accept="image/png, image/jpeg"
+                />
+
+                <Button type="submit" variation="primary">
+                  Create Note
                 </Button>
               </Flex>
-            ))}
-          </Grid>
-          <Button onClick={signOut}>Sign Out</Button>
-        </Flex>
-      )}
-    </Authenticator>
+            </View>
+            <Divider />
+            <Heading level={2}>Current Notes</Heading>
+            <Grid
+              margin="3rem 0"
+              autoFlow="column"
+              justifyContent="center"
+              gap="2rem"
+              alignContent="center"
+            >
+              {notes.map((note) => (
+                <Flex
+                  key={note.id || note.name}
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  gap="2rem"
+                  border="1px solid #ccc"
+                  padding="2rem"
+                  borderRadius="5%"
+                  className="box"
+                >
+                  <View>
+                    <Heading level="3">{note.name}</Heading>
+                  </View>
+                  <Text fontStyle="italic">{note.description}</Text>
+                  {note.image && (
+                    <Image
+                      src={note.image}
+                      alt={`visual aid for ${notes.name}`}
+                      style={{ width: 400 }}
+                    />
+                  )}
+                  <Button
+                    variation="destructive"
+                    onClick={() => deleteNote(note)}
+                  >
+                    Delete note
+                  </Button>
+                </Flex>
+              ))}
+            </Grid>
+            <Button onClick={signOut}>Sign Out</Button>
+          </Flex>
+        )}
+      </Authenticator>
+    </ThemeProvider>
   );
 }
