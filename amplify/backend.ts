8923d6488b2fd025ed7fec0b275340bc6ab3130e
PJ Neary <pneary@pathward.com>
2024-10-24 15:22:52 -0500

    Fix auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,10 +6,5 @@ import { data } from './data/resource';
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  auth,
-  data,
+  auth
 });
-
-const { cfnUserPool } = backend.auth.resources.cfnResources
-
-cfnUserPool.usernameAttributes = []
--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,20 @@
-import { useState, useEffect } from "react";
 import {
   Authenticator,
-  Button,
-  Text,
-  TextField,
-  Heading,
-  Flex,
   View,
-  Image,
-  Grid,
-  Divider,
   ThemeProvider,
   useTheme,
 } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { getUrl } from "aws-amplify/storage";
-import { uploadData } from "aws-amplify/storage";
-import { generateClient } from "aws-amplify/data";
+import outputs from '../amplify_outputs.json';
 
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
 
-Amplify.configure({
-  Auth: {
-
-
-    // REQUIRED - Amazon Cognito Region
-    region: 'us-east-1',
-
-    // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: 'us-east-1_SjmJkqcBy',
-
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
-
-    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-    mandatorySignIn: false,
-
-    // OPTIONAL - Configuration for cookie storage
-    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    cookieStorage: {
-    // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-        domain: '.amplifyapp.com',
-    // OPTIONAL - Cookie path
-        path: '/',
-    // OPTIONAL - Cookie expiration in days
-        expires: 365,
-    // OPTIONAL - Cookie secure flag
-    // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-        secure: true
-    },
-
-    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    authenticationFlowType: 'USER_PASSWORD_AUTH'
-  }
-});
-
-const client = generateClient({
-  authMode: "userPool",
-});
+Amplify.configure(outputs);
 
 export default function App() {
-  const [notes, setNotes] = useState([]);
 
   const { tokens } = useTheme();
   const theme = {
@@ -105,158 +56,15 @@ export default function App() {
     },
   };
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const { data: notes } = await client.models.Note.list();
-    await Promise.all(
-      notes.map(async (note) => {
-        if (note.image) {
-          const linkToStorageFile = await getUrl({
-            path: ({ identityId }) => `media/${identityId}/${note.image}`,
-          });
-          console.log(linkToStorageFile.url);
-          note.image = linkToStorageFile.url;
-        }
-        return note;
-      })
-    );
-    console.log(notes);
-    setNotes(notes);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    console.log(form.get("image").name);
-
-    const { data: newNote } = await client.models.Note.create({
-      name: form.get("name"),
-      description: form.get("description"),
-      image: form.get("image").name,
-    });
-
-    console.log(newNote);
-    if (newNote.image)
-      if (newNote.image)
-        await uploadData({
-          path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
-
-          data: form.get("image"),
-        }).result;
-
-    fetchNotes();
-    event.target.reset();
-  }
-
-  async function deleteNote({ id }) {
-    const toBeDeletedNote = {
-      id: id,
-    };
-
-    const { data: deletedNote } = await client.models.Note.delete(
-      toBeDeletedNote
-    );
-    console.log(deletedNote);
-
-    fetchNotes();
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <View padding="xxl"></View>
-      <Authenticator loginMechanisms={['username']} variation="modal" hideSignUp>
-        {({ signOut }) => (
-          <Flex
-            className="App"
-            justifyContent="center"
-            alignItems="center"
-            direction="column"
-            width="70%"
-            margin="0 auto"
-          >
-            <Heading level={1}>My Notes App</Heading>
-            <View as="form" margin="3rem 0" onSubmit={createNote}>
-              <Flex
-                direction="column"
-                justifyContent="center"
-                gap="2rem"
-                padding="2rem"
-              >
-                <TextField
-                  name="name"
-                  placeholder="Note Name"
-                  label="Note Name"
-                  labelHidden
-                  variation="quiet"
-                  required
-                />
-                <TextField
-                  name="description"
-                  placeholder="Note Description"
-                  label="Note Description"
-                  labelHidden
-                  variation="quiet"
-                  required
-                />
-                <View
-                  name="image"
-                  as="input"
-                  type="file"
-                  alignSelf={"end"}
-                  accept="image/png, image/jpeg"
-                />
-
-                <Button type="submit" variation="primary">
-                  Create Note
-                </Button>
-              </Flex>
-            </View>
-            <Divider />
-            <Heading level={2}>Current Notes</Heading>
-            <Grid
-              margin="3rem 0"
-              autoFlow="column"
-              justifyContent="center"
-              gap="2rem"
-              alignContent="center"
-            >
-              {notes.map((note) => (
-                <Flex
-                  key={note.id || note.name}
-                  direction="column"
-                  justifyContent="center"
-                  alignItems="center"
-                  gap="2rem"
-                  border="1px solid #ccc"
-                  padding="2rem"
-                  borderRadius="5%"
-                  className="box"
-                >
-                  <View>
-                    <Heading level="3">{note.name}</Heading>
-                  </View>
-                  <Text fontStyle="italic">{note.description}</Text>
-                  {note.image && (
-                    <Image
-                      src={note.image}
-                      alt={`visual aid for ${notes.name}`}
-                      style={{ width: 400 }}
-                    />
-                  )}
-                  <Button
-                    variation="destructive"
-                    onClick={() => deleteNote(note)}
-                  >
-                    Delete note
-                  </Button>
-                </Flex>
-              ))}
-            </Grid>
-            <Button onClick={signOut}>Sign Out</Button>
-          </Flex>
+      <Authenticator variation="modal" hideSignUp>
+        {({ signOut, user }) => (
+          <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
         )}
       </Authenticator>
     </ThemeProvider>
